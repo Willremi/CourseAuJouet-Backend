5a3ffcf3993fe1d85d1cbcac82f182a1abed7326
Adrien Boudet <adrien.boudet1@gmail.com>
2021-11-18 10:14:29 +0100

    auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,20 +5,13 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];;
         const payload = jwt.decode(token , 'RANDOM_SECRET_TOKEN');
-        //const user = User.findOne({id: payload.userId })
-        //console.log(user);
         // on vérifie que le payload JWT renvoi bien un ID 
-        // et que l'expiration n'est pas dépassée
         if (!payload.userId ) {
             throw 'user ID non valable';
-        } else if ( Date.now() >= payload.exp * 1000 ) {
-           throw 'Token de connection expiré'
         } else {
             next();
         }
     } catch(error) {
-        res.status(401).json({ error: error | 'requête non authentifiée'});
+        res.status(401).json({ error: 'requête non authentifiée'});
     }
 }
-
-// || user.rememberMe !== payload.rememberMe 
