e57396288ac27b64a6cdb0040c5c075d32c05046
Mathieu Bidoyen <m.bidoyen@gmail.com>
2021-12-10 15:00:22 +0100

    Affinage de la recherche lors du click sur le nom d'un produit dans la liste déroulante de l'autocomplétion

--- controllers/search.js
+++ controllers/search.js
@@ -4,7 +4,7 @@ exports.searchProduct = (req, res, next) => {
   
   Product.aggregate([{
       '$search': {
-        'index': 'Submit Search',
+        'index': 'Result Search Submit',
         'text': {
           'query': `${req.body.search}`,
           path: {
@@ -30,10 +30,10 @@ exports.autoCompleteSearch = (req, res, next) => {
         "autocomplete": {//autocompletion
           "path": 'product_name',//nom du champs
           "query": `${req.params.text}`,// élément recherché
-          "fuzzy": {//Active la recherche floue. Recherche des chaînes similaires au ou aux termes recherchés.
-            "maxEdits": 2,//Nombre maximal de modifications de caractère unique requises pour correspondre au terme de recherche spécifié. La valeur peut être 1ou 2.
-            "prefixLength": 2,//Nombre de caractères au début de chaque terme dans le résultat qui doit correspondre exactement. La valeur par défaut est 0.
-          }
+          // "fuzzy": {//Active la recherche floue. Recherche des chaînes similaires au ou aux termes recherchés.
+          //   "maxEdits": 2,//Nombre maximal de modifications de caractère unique requises pour correspondre au terme de recherche spécifié. La valeur peut être 1ou 2.
+          //   "prefixLength": 2,//Nombre de caractères au début de chaque terme dans le résultat qui doit correspondre exactement. La valeur par défaut est 0.
+          // }
         },
       }
     }])
@@ -45,4 +45,31 @@ exports.autoCompleteSearch = (req, res, next) => {
     .catch((error) => res.status(500).json({
       error
     }))
+}
+
+exports.onSelectedAutoCompletion = (req, res, next) => {
+
+  Product.aggregate(
+    [
+  {
+    $search: {
+      index: 'Select Product On AutoCompletion List',
+      text: {
+        query: `${req.body.search}`,
+        path: {
+          'wildcard': '*'
+        }
+      }
+    }
+  }
+]
+  )
+  .then((result) => {
+    res.status(200).json({
+      result
+    })
+  })
+  .catch((error) => res.status(404).json({
+    error
+  }))
 }
