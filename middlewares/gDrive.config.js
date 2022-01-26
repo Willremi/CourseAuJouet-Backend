8d1170b2c99505cdd6e3e3f9fc3f0cab28520ade
Rémi Willebrouck <willremi@gmail.com>
2022-01-26 10:30:02 +0100

    travail sur le middleware pour config googleDrive

--- middlewares/gDrive.config.js
+++ middlewares/gDrive.config.js
@@ -1,4 +1,4 @@
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const OAuth2Data = require('../credentials.json');
 
 const CLIENT_ID = OAuth2Data.web.client_id;
@@ -18,9 +18,65 @@ const SCOPES = "https://www.googleapis.com/auth/drive.file https://www.googleapi
 //     version: "v2"
 // })
 
-module.exports.uploadDrive = (res, req) => {
-    const oauth2 = google.oauth2({
-        auth: oAuth2Client,
-        version: "v2"
-    })
+const drive = google.drive({
+    version: 'v3',
+    auth: oAuth2Client
+})
+
+// Création de dossier dans le dossier
+// var folderMetadata = {
+//     'name': '"nom du dossier"',
+//     'mimeType': 'application/vnd.google-apps.folder'
+// };
+
+// drive.files.create({
+//     resource: folderMetadata,
+//     fields: { id: 'id', name: 'name' }
+// }, function (err, file) {
+//     if (err) {
+//         // Handle error
+//         console.error(err);
+//     } else {
+//         console.log('Folder Id: ', file.data.id);
+//         const folderId = file.data.id
+
+//         // Uploader des images dans dossier
+
+//         const filemetadata = {
+//             name: nomSlice,
+//             parents: [folderId]
+//         }
+
+//         const media = {
+//             mimeType: value.mimetype,
+//             body: fs.createReadStream(value.path)
+//         }
+
+//         drive.files.create({
+//             resource: filemetadata,
+//             media: media,
+//             fields: 'id'
+//         }, (err, file) => {
+//             if (err) throw err
+//             // console.log(file.data.id);
+//             let fileId = file.data.id
+
+//             // partager pour être vu par tous
+//             drive.permissions.create({
+//                 fileId: fileId,
+//                 requestBody: {
+//                     role: 'reader',
+//                     type: 'anyone'
+//                 }
+//             })
+//             drive.files.get({
+//                 fileId,
+//                 fields: 'webViewLink'
+//             })
+
+//         })
+//     }
+// });
+
+module.exports.uploadDrive = () => {
 }
